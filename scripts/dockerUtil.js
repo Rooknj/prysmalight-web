dd1f2e8792d754beffd91cfad361d8c196dfc209
Nick Rook <rooknj@outlook.com>
2019-02-05 02:07:58 -0500

    enabled docker scripts

--- scripts/dockerUtil.js
+++ scripts/dockerUtil.js
@@ -3,6 +3,8 @@ const child_process = require("child_process");
 const exec = util.promisify(child_process.exec);
 const parseArgs = require("minimist");
 
+const IMAGE_NAME = "rooknj/prysmalight-web";
+
 /**
  * Executes a command then prints STDOUT and STDERR.
  * Any command that errors will cause the process to exit with error code 1.
@@ -29,57 +31,32 @@ const executeCommand = async (command, cwd = null) => {
   }
 };
 
-const buildDockerImage = async (pkg, tag) => {
-  const path = `./packages/${pkg}`;
-  const image = `rooknj/prysmalight-${pkg}:${tag}`;
-  const latest = `rooknj/prysmalight-${pkg}:latest`;
-  console.log("Pulling", latest);
-  await executeCommand(`docker pull ${latest}`);
+const getDockerImage = tag => {
+  let dockerTag = tag;
+  if (process.env.TRAVIS) {
+    console.log("In CI");
+    const branchName = process.env.TRAVIS_BRANCH;
+    if (branchName) {
+      if (branchName === "master") {
+        dockerTag = "latest";
+      } else {
+        dockerTag = "test";
+      }
+    }
+  }
+  return `${IMAGE_NAME}:${dockerTag}`;
+};
+
+const buildDockerImage = async tag => {
+  const image = getDockerImage(tag);
   console.log("Building", image);
-  await executeCommand(
-    `docker build --cache-from ${latest} -t ${image} ${path}`
-  );
+  await executeCommand(`docker build -t ${image} .`);
 };
 
-const publishDockerImage = async (pkg, tag) => {
-  await executeCommand(`docker push rooknj/prysmalight-${pkg}:${tag}`);
-};
-
-// Builds the package (Note: using -t local. Change docker build so i dont need to include a tag for building)
-const buildPackage = async (packageName, tag) => {
-  if (!process.env.TRAVIS || packageWasChanged(packageName)) {
-    const branchName = process.env.TRAVIS_BRANCH;
-    if (branchName) {
-      if (branchName === "master") {
-        tag = "latest";
-      } else {
-        tag = "test";
-      }
-    }
-    buildDockerImage(packageName, tag);
-  } else {
-    console.log(
-      `Currently in CI and ${packageName} was not changed. Skipping build`
-    );
-  }
-};
-
-const deployPackage = async (packageName, tag) => {
-  if (!process.env.TRAVIS || packageWasChanged(packageName)) {
-    const branchName = process.env.TRAVIS_BRANCH;
-    if (branchName) {
-      if (branchName === "master") {
-        tag = "latest";
-      } else {
-        tag = "test";
-      }
-    }
-    publishDockerImage(packageName, tag);
-  } else {
-    console.log(
-      `Currently in CI and ${packageName} was not changed. Skipping deploy`
-    );
-  }
+const publishDockerImage = async tag => {
+  const image = getDockerImage(tag);
+  console.log("Publishing", image);
+  await executeCommand(`docker push ${image}`);
 };
 
 // Process all command line arguments
@@ -89,8 +66,15 @@ const processArgs = async args => {
     console.log("Not in CI and no tag was given. Aborting");
     process.exit(1);
   }
-    packages.forEach(pack => buildPackage(pack, args.t));
-  
+
+  if (args._.find(arg => arg === "build")) {
+    buildDockerImage(args.t);
+  } else if (args._.find(arg => arg === "publish")) {
+    publishDockerImage(args.t);
+  } else {
+    console.log("No valid options supplied. Aborting");
+    process.exit(1);
+  }
 };
 
 // Returns an array of objects in the shape of:
