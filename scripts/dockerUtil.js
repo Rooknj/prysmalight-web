f897d1c5d28a4f57f1c20c63afde8cbb4e3282eb
Nick Rook <rooknj@outlook.com>
2019-04-14 17:50:56 -0400

    edited dockerUtil to include versioning

--- scripts/dockerUtil.js
+++ scripts/dockerUtil.js
@@ -1,35 +1,9 @@
-const util = require("util");
-const child_process = require("child_process");
-const exec = util.promisify(child_process.exec);
 const parseArgs = require("minimist");
+const spawn = require("cross-spawn");
+const packageJson = require("../package.json");
 
 const IMAGE_NAME = "rooknj/prysmalight-web";
-
-/**
- * Executes a command then prints STDOUT and STDERR.
- * Any command that errors will cause the process to exit with error code 1.
- * @param {*} command
- * @param {*} cwd
- */
-const executeCommand = async (command, cwd = null) => {
-  console.log(`Executing: ${command} in ${cwd || "."}`);
-  try {
-    const { stdout, stderr } = await exec(command, { cwd });
-    console.log(`Finished: ${command} in ${cwd || "."}`);
-    console.log("STDOUT:");
-    console.log(stdout || "None");
-    console.log("STDERR:");
-    console.log(stderr || "None");
-    return stdout;
-  } catch (error) {
-    console.log(`Error Executing: ${command} in ${cwd || "."}`);
-    console.log("STDOUT:");
-    console.log(error.stdout || "None");
-    console.log("STDERR:");
-    console.log(error.stderr || "None");
-    process.exit(1);
-  }
-};
+const VERSION = packageJson.version;
 
 const getDockerImage = tag => {
   let dockerTag = tag;
@@ -38,7 +12,7 @@ const getDockerImage = tag => {
     const branchName = process.env.TRAVIS_BRANCH;
     if (branchName) {
       if (branchName === "master") {
-        dockerTag = "latest";
+        dockerTag = VERSION;
       } else {
         dockerTag = "test";
       }
@@ -50,13 +24,27 @@ const getDockerImage = tag => {
 const buildDockerImage = async tag => {
   const image = getDockerImage(tag);
   console.log("Building", image);
-  await executeCommand(`docker build -t ${image} .`);
+  const child = spawn("docker", ["build", "-t", image, "."], {
+    stdio: ["inherit", "inherit", "inherit"],
+    cwd: process.cwd(),
+    env: process.env
+  });
+  child.on("exit", code => {
+    if (code !== 0) process.exit(code);
+  });
 };
 
 const publishDockerImage = async tag => {
   const image = getDockerImage(tag);
   console.log("Publishing", image);
-  await executeCommand(`docker push ${image}`);
+  const child = spawn("docker", ["push", image], {
+    stdio: ["inherit", "inherit", "inherit"],
+    cwd: process.cwd(),
+    env: process.env
+  });
+  child.on("exit", code => {
+    if (code !== 0) process.exit(code);
+  });
 };
 
 // Process all command line arguments
