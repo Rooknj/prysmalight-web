b8068fe784d64c40d7a476e1c675dd1cb48335c2
Nick Rook <rooknj@outlook.com>
2019-04-14 20:48:59 -0400

    split out discoveredLightsList

--- src/Components/LightAppBar/LightActions/AddLightButton/AddLightDialog.jsx
+++ src/Components/LightAppBar/LightActions/AddLightButton/AddLightDialog.jsx
@@ -1,16 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Button from "@material-ui/core/Button";
-import Avatar from "@material-ui/core/Avatar";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemAvatar from "@material-ui/core/ListItemAvatar";
-import ListItemText from "@material-ui/core/ListItemText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Dialog from "@material-ui/core/Dialog";
-import PersonIcon from "@material-ui/icons/Person";
-import AddIcon from "@material-ui/icons/Add";
-import Typography from "@material-ui/core/Typography";
+import DiscoveredLightsList from "./DiscoveredLightsList";
 
 const emails = ["username@gmail.com", "user02@gmail.com"];
 
@@ -33,30 +25,10 @@ function SimpleDialog(props) {
     >
       <DialogTitle id="simple-dialog-title">Set backup account</DialogTitle>
       <div>
-        <List>
-          {emails.map(email => (
-            <ListItem
-              button
-              onClick={() => handleListItemClick(email)}
-              key={email}
-            >
-              <ListItemAvatar>
-                <Avatar>
-                  <PersonIcon />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText primary={email} />
-            </ListItem>
-          ))}
-          <ListItem button onClick={() => handleListItemClick("addAccount")}>
-            <ListItemAvatar>
-              <Avatar>
-                <AddIcon />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText primary="add account" />
-          </ListItem>
-        </List>
+        <DiscoveredLightsList
+          lights={emails}
+          onListItemClick={handleListItemClick}
+        />
       </div>
     </Dialog>
   );
